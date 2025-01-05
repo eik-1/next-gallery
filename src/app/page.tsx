d0edea0608f2f7aa27cedc8f8be5607d6292b158
Sarthak <sarthakrawatbiz@gmail.com>
2025-01-05 14:06:56 +0530

    Use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,40 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const revalidate = 0;
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image, index) => (
+        <div key={image.id + "-" + index} className="flex h-48 w-48 flex-col">
+          <img
+            src={image.url}
+            alt="image"
+            className="h-full w-full object-cover"
+          />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image, index) => (
-          <div key={image.id + "-" + index} className="flex h-48 w-48 flex-col">
-            <img
-              src={image.url}
-              alt="image"
-              className="h-full w-full object-cover"
-            />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please Sign In Above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
